6263d83f66f4267df749d2cfc3bd73e16e9db60d
Mikhail Stankevich <mikhailstn1@gmail.com>
2023-10-20 17:36:40 +0300

    feat: add footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,5 @@
 import "./footer.scss";
+import { Button } from "@mui/material";
 // import { Link } from "react-router-dom";
 // import { useAppDispatch } from "../../app/hooks";
 // import { setActiveHome, setActiveStore, setActiveBlog } from "../../store/headerNav";
@@ -30,7 +31,73 @@ export function Footer() {
 
   return (
     <div className="footer">
-      <div className="footer__content"></div>
+      <div className="footer__content">
+        <div className="footer__blocks">
+          <div className="footer__block">
+            <h2 className="footer__block_title">Company</h2>
+            <ul className="footer__block_list">
+              <li className="footer__block__li">
+                <a className="footer__block_link">About us</a>
+              </li>
+              <li className="footer__block__li">
+                <a className="footer__block_link">News</a>
+              </li>
+              <li className="footer__block__li">
+                <a className="footer__block_link">Politics</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__block">
+            <h2 className="footer__block_title">Information</h2>
+            <ul className="footer__block_list">
+              <li className="footer__block__li">
+                <a className="footer__block_link">Help</a>
+              </li>
+              <li className="footer__block__li">
+                <a className="footer__block_link">Payment terms</a>
+              </li>
+              <li className="footer__block__li">
+                <a className="footer__block_link">Delivery terms</a>
+              </li>
+              <li className="footer__block__li">
+                <a className="footer__block_link">Exchange/Return</a>
+              </li>
+            </ul>
+          </div>{" "}
+          <div className="footer__block">
+            <h2 className="footer__block_title">Help</h2>
+            <ul className="footer__block_list">
+              <li className="footer__block__li">
+                <a className="footer__block_link">Blog</a>
+              </li>
+              <li className="footer__block__li">
+                <a className="footer__block_link">Brands</a>
+              </li>
+              <li className="footer__block__li">
+                <a className="footer__block_link">FAQ</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__block">
+            <h2 className="footer__block_title">Subscribe us</h2>
+            <form className="footer__form">
+              <input required type="email" name="email" className="footer__block_input" placeholder="E-mail"></input>
+              <Button variant="outlined" className="footer__block_button" type="submit">
+                Subscribe
+              </Button>
+            </form>
+          </div>
+          <div className="footer__block">
+            <h2 className="footer__block_title">Social Media</h2>
+            <div className="footer__social">
+              <a className="footer__social_link youtube" href="#"></a>
+              <a className="footer__social_link telegram"></a>
+              <a className="footer__social_link vk"></a>
+            </div>
+          </div>
+        </div>
+        <p className="footer__date">{new Date().getFullYear()}</p>
+      </div>
     </div>
   );
 }
